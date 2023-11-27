17b92ead75fe644a0fd59f3be6d4ed33c887edbc
Linashem <lina.shemeneva@yandex.ru>
2023-11-28 02:16:14 +0300

    пагинация

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -6,10 +6,13 @@ export const TodoList: React.FC = () => {
   const { error, limit, loading, page, todos } = useTypedSelector(
     (state) => state.todo
   );
-  const {fetchTodos}=useActions();
-useEffect(()=>{
-    fetchTodos(page, limit)
-},[])
+  const { fetchTodos,setTodoPage } = useActions();
+
+  const pages = [1, 2, 3, 4, 5];
+
+  useEffect(() => {
+    fetchTodos(page, limit);
+  }, [page]);
 
   if (loading) {
     return <h1>Loading...</h1>;
@@ -17,8 +20,27 @@ useEffect(()=>{
   if (error) {
     return <h1>Some problems</h1>;
   }
-  return <div>
-    <hr />
-    {todos.map((todo)=> <div key={todo.id}>{todo.title}</div> )}
-  </div>;
+  return (
+    <div>
+      <div>
+        <hr />
+        {todos.map((todo) => (
+          <div key={todo.id}>{todo.id}-{todo.title}</div>
+        ))}
+      </div>
+      <div style={{ display: "flex" }}>
+        {pages.map((p) => (
+          <div
+            style={{
+              border: p === page ? "2px solid green" : "1px solid grey",
+              padding: "10px",
+            }}
+            onClick={() => setTodoPage(p)}
+          >
+            {p}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
